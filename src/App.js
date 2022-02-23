23a7e071e619a2ef6a95cd97ea25e877477ea0ee
Roman Plotnikov <rekss13@gmail.com>
2022-02-23 17:44:14 +0300

    added test socket io client

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import ClientComponent from "./ClientComponent";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [loadClient, setLoadClient] = useState(true);
+
+    return (
+        <>
+            {/* LOAD OR UNLOAD THE CLIENT */}
+            <button onClick={() => setLoadClient(prevState => !prevState)}>
+                STOP CLIENT
+            </button>
+            {/* SOCKET IO CLIENT*/}
+            {loadClient ? <ClientComponent /> : null}
+        </>
+    );
 }
 
 export default App;
---
+++ src/ClientComponent.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+import socketIOClient from "socket.io-client";
+const ENDPOINT = "http://127.0.0.1:4001";
+
+const ClientComponent = () => {
+    const [response, setResponse] = useState("");
+
+    useEffect(() => {
+        const socket = socketIOClient(ENDPOINT);
+        socket.on("FromAPI", data => {
+            setResponse(data);
+        });
+
+    }, []);
+
+    return (
+        <p>
+            It's <time dateTime={response}>{response}</time>
+        </p>
+    );
+}
+
+export default ClientComponent;
